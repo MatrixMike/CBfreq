1866fad8952cf04c692e3c1204ef47f45245af5c
Mike Hewitt <matroxmike@icloud.com>
2017-01-30 07:35:29 +1100

    properties of strCreate class

--- strCreate.java
+++ strCreate.java
@@ -1,5 +1,9 @@
 public  class strCreate  {
     // was public  class strCreate  extends CBfreq{
+    // so need to find out the pros and cons of 'extends other class'
+    // https://docs.oracle.com/javase/tutorial/java/IandI/subclasses.html
+    // https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
+    // https://docs.oracle.com/javase/tutorial/getStarted/problems/index.html
     static   String make1 (int chn, int f ){
         String message1 = "";
         StringBuilder sb1 = new StringBuilder(message1);
